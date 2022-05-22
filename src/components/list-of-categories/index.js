958dab8eba94071a4eea7133e2cdd131794851b9
gerard2perez <gerard2perez@outlook.com>
2022-05-21 21:50:37 -0500

    feat(class-10): import values from db.json

--- src/components/list-of-categories/index.js
+++ src/components/list-of-categories/index.js
@@ -5,18 +5,21 @@ Learning Resource from platzi
 
 File: index.js
 Created:  2022-05-22T01:55:05.818Z
-Modified: 2022-05-22T02:01:02.134Z
+Modified: 2022-05-22T02:50:00.305Z
 */
 
 import React from 'react'
+import db from '../../../api/db.json'
 import { Category } from '../category'
 import { Item, List } from './styles'
+console.log(db)
 export const ListOfCategories = () => {
+  const categories = db.categories
   return (
     <List>
       {
-        [1,2,3,4].map((id) =>(
-        <Item key={id}><Category/></Item>
+        categories.map((category) =>(
+        <Item key={category.id}><Category {...category}/></Item>
         ))
       }
     </List>
