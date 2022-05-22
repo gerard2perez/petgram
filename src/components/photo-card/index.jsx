8fe9746d8a5df4689d62b9674510030a2e1f7577
gerard2perez <gerard2perez@outlook.com>
2022-05-22 12:39:11 -0500

    chore(class-19): uses localstorage for likes

--- src/components/photo-card/index.jsx
+++ src/components/photo-card/index.jsx
@@ -5,16 +5,25 @@ Learning Resource from platzi
 
 File: index.js
 Created:  2022-05-22T02:53:20.673Z
-Modified: 2022-05-22T17:17:48.288Z
+Modified: 2022-05-22T17:38:16.229Z
 */
 
 import React, { useEffect, useRef, useState } from 'react'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavorite, MdFavoriteBorder } from 'react-icons/md'
 import { Article, Button, Img, ImgWrapper } from './styles'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
   const element = useRef(null)
   const [show, setShow] = useState(false)
+  const key = `liked-${id}`
+  const [liked, setLiked] = useState(()=>{
+    try{
+    const like = window.localStorage.getItem(key)
+    return like
+    }catch(e) {
+      console.error(e)
+    }
+  })
   useEffect(()=>{
     const observer = new window.IntersectionObserver(function(entries) {
       const { isIntersecting } = entries[0]
@@ -26,6 +35,15 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
     })
     observer.observe(element.current)
   }, [element])
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+  const handleLiked = (liked) =>{
+    try {
+      window.localStorage.setItem(key, liked)
+    } catch(e) {
+      console.error(e)
+    }
+    setLiked(liked)
+  }
   return (
     <Article ref={element}>
       {show && <>
@@ -34,8 +52,8 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
             <Img src={src}/>
           </ImgWrapper>
         </a>
-        <Button>
-          <MdFavoriteBorder size='32px'/> {likes} likes!
+        <Button onClick={()=>handleLiked(!liked)}>
+          <Icon size='32px'/> {likes} likes!
         </Button>
       </>}
 
