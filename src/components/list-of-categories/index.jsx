371ba10576ac1f995a28b19694b13a6f3f1293b9
gerard2perez <gerard2perez@outlook.com>
2022-05-22 09:38:22 -0500

    feat(class-14): takes categories from backend

--- src/components/list-of-categories/index.jsx
+++ src/components/list-of-categories/index.jsx
@@ -5,16 +5,20 @@ Learning Resource from platzi
 
 File: index.js
 Created:  2022-05-22T01:55:05.818Z
-Modified: 2022-05-22T03:05:34.644Z
+Modified: 2022-05-22T14:37:09.774Z
 */
 
-import React from 'react'
-import db from '../../../api/db.json'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../category'
 import { Item, List } from './styles'
 
 export const ListOfCategories = () => {
-  const categories = db.categories
+  const [categories, useCategories] = useState([])
+  useEffect(()=>{
+    fetch('https://petgram-server-g2p.vercel.app/categories')
+    .then(res=>res.json())
+    .then(response => useCategories(response));
+  }, [])
   return (
     <List>
       {
